5f918b5df190a8e1cc4e3e7cbb6c9ac5a3dd5833
owen.wu <owen.wu@arbaholdings.com.hk>
2019-08-27 14:24:57 +0800

    dd

---
+++ src/main/java/com.arba/Test.java
@@ -0,0 +1,10 @@
+package com.arba;
+
+/**
+ * Created by dell on 2019/8/27.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
